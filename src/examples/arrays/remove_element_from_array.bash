7da3004a7daa4376f70bb95f4c2aac268aeab6a7
Mark Veltzer <mark.veltzer@gmail.com>
2017-08-05 22:51:59 +0300

    yet another example

---
+++ src/examples/arrays/remove_element_from_array.bash
@@ -0,0 +1,7 @@
+#!/bin/bash -u
+
+# This is an example of how to remove an element from an array in bash.
+# References:
+# - https://stackoverflow.com/questions/16860877/remove-element-from-array-shell
+
+# TBD
