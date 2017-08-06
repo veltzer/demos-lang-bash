86f8d8915168542eea084c090e49a250fd87bdb1
Mark Veltzer <mark.veltzer@gmail.com>
2017-08-06 21:49:36 +0300

    better example

--- src/examples/functions/pass_values_to.bash
+++ src/examples/functions/pass_values_to.bash
@@ -1,9 +1,36 @@
 #!/bin/bash -u
 
 # This is how to pass values to a function
+#
+# NOTES:
+# - it is recommended to assign argument to local
+# variables declared with 'local' right at function start.
+# - there is no reason to surround the arguments ($1, $2, ...)
+# with quotes when doing this assignment.
+# - when using the argument aside from the assignment to local
+# variables they must be handled with care, as with any
+# bash variable.
 
-function print_arg {
-	echo $1
+function func1 {
+	local arg="$1"
+	echo "arg is [$arg]"
 }
 
-print_arg hello
+function func2 {
+	local arg=$1
+	echo "arg is [$arg]"
+}
+
+function func3 {
+	arg=$1
+	echo "arg is [$arg]"
+}
+
+function func4 {
+	echo "arg is [$1]"
+}
+
+func1 "hello world"
+func2 "hello world"
+func3 "hello world"
+func4 "hello world"
