d3b77738bd9a1035ea67bdc6abe1ee2b919bc3fd
Mark Veltzer <mark.veltzer@gmail.com>
2017-08-26 16:00:08 +0300

    trap example

---
+++ src/examples/errors/trap.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This is an example of how to handle bash error using 'trap' builtin.
+#
+# References:
+# - https://stackoverflow.com/questions/64786/error-handling-in-bash
+ 
+# TBD
