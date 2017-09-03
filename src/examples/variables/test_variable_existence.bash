95406409db49b4731cdfb3d36278de99e45e4c39
Mark Veltzer <mark.veltzer@gmail.com>
2017-09-03 19:37:49 +0300

    get rid of common.bash

--- src/examples/variables/test_variable_existence.bash
+++ src/examples/variables/test_variable_existence.bash
@@ -3,54 +3,50 @@
 # This is an example that show how to test whether a variable is set or not.
 #
 # References:
-# http://stackoverflow.com/questions/3601515/how-to-check-if-a-variable-is-set-in-bash
+# - http://stackoverflow.com/questions/3601515/how-to-check-if-a-variable-is-set-in-bash
 
-source ../../includes/common.bash
-
-if ! declare -p myvar 2> /dev/null > /dev/null
-then
-	echo "yes, myvar is not set"
-fi
-myvar=7
 if declare -p myvar 2> /dev/null > /dev/null
 then
-	echo "yes, myvar is set"
+	echo "ERROR"
+fi
+myvar=7
+if ! declare -p myvar 2> /dev/null > /dev/null
+then
+	echo "ERROR"
 fi
 
 # test if 'somevar' is not set
-if [ -z ${somevar+x} ]
+if ! [ -z ${somevar+x} ]
 then
-	echo "yes, somevar is not set"
+	echo "ERROR"
 fi
 somevar=7
 # test if 'somevar' is set
-if [ -n "${somevar}" ]
+if ! [ -n "${somevar}" ]
 then
-	echo "yes, somevar is set"
+	echo "ERROR"
 fi
 # even shorter, -n is the default test
-if [ "${somevar}" ]
+if ! [ "${somevar}" ]
 then
-	echo "yes, somevar is set"
+	echo "ERROR"
 fi
 unset somevar
 # this should not happen
 if [ "${somevar-}" ]
 then 
-	die "problem 1"
-else
-	echo "yes, somevar is not set"
+	echo "ERROR"
 fi
 
 if [ ! -z ${unsetvar+x} ]
 then
-	die "problem 2"
+	echo "ERROR"
 fi
 if [ -n "${unsetvar-}" ]
 then
-	die "problem 3"
+	echo "ERROR"
 fi
 if [ ! -n ${somevar+x} ]
 then
-	die "problem 4"
+	echo "ERROR"
 fi
