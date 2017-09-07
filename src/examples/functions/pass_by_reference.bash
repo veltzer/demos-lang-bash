42c2cdc7f99d39ec7f52e1ac959ef21de55be5c1
Mark Veltzer <mark.veltzer@gmail.com>
2017-09-07 17:47:04 +0300

    more fixes to the code

--- src/examples/functions/pass_by_reference.bash
+++ src/examples/functions/pass_by_reference.bash
@@ -1,11 +1,67 @@
 #!/bin/bash -ue
 
-# This example shows how to pass variables by reference to bash function
-# corretly. This is trickier than it sounds.
+# This example shows how to pass values by reference
+# to bash functions
 #
-# References:
-# - http://www.fvue.nl/wiki/Bash:_Passing_variables_by_reference
+# There are several ways to pass arrays by reference
+# to a bash function:
+# - passing the name of the variable and using eval
+# to assign to it.
+# - passing the name of the variable and using local -n
+# to receive it.
+# method 2 sounds nicer.
 
-f() { local b; g b; echo $b; }
-g() { local b; eval $1=bar; }  #  WRONG, although it looks ok: b=bar
-f
+source src/includes/array.bashinc
+
+function using_eval() {
+	local var=$1
+	local value=$2
+	eval "$var=$value"
+}
+
+function using_minus_n() {
+	local -n var=$1
+	local value=$2
+	var=$value
+}
+
+function array_with_minus_n() {
+	local -n var=$1
+	var=(1 2 3)
+}
+
+a=5
+using_eval a 6
+if [ "$a" != 6 ]
+then
+	echo "ERROR 1"
+fi
+
+# show that this method has problem with same named variables
+var=6
+using_eval var 7
+if [ "$var" != 6 ]
+then
+	echo "ERROR 2"
+fi
+
+a=7
+using_minus_n a 8
+if [ "$a" != 8 ]
+then
+	echo "ERROR 3"
+fi
+
+a=3
+array_with_minus_n a
+if ! array_is_array a
+then
+	echo "ERROR 4"
+fi
+
+var=9
+using_minus_n var 10
+if [ "$var" != 10 ]
+then
+	echo "ERROR 5"
+fi
---
+++ src/examples/functions/pass_by_reference_problems_with_eval.bash
@@ -0,0 +1,11 @@
+#!/bin/bash -ue
+
+# This example shows how to pass variables by reference to bash function
+# corretly. This is trickier than it sounds.
+#
+# References:
+# - http://www.fvue.nl/wiki/Bash:_Passing_variables_by_reference
+
+f() { local b; g b; echo $b; }
+g() { local b; eval $1=bar; }  #  WRONG, although it looks ok: b=bar
+f
