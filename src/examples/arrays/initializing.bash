f3e6abeb9ff55a5e20c4575477845273efbadc31
Mark Veltzer <mark.veltzer@gmail.com>
2017-09-07 14:17:32 +0300

    now

--- src/examples/arrays/initializing.bash
+++ src/examples/arrays/initializing.bash
@@ -17,38 +17,40 @@
 # References:
 # - http://tldp.org/LDP/abs/html/arrays.html
 
+source src/includes/array.bashinc
+
 # declare an array (it will be empty)
 declare -A my_array1
-echo "my_array1 ${my_array1[@]}"
+array_print my_array1
 
 # declare an array and put data
 declare -A my_array2[0]=0
 declare -A my_array2[1]=1
 declare -A my_array2[2]=2
-echo "my_array2 ${my_array2[@]}"
+array_print my_array2
 
 # declare an array and then assign
 declare -A my_array3
 my_array3[0]=0
 my_array3[1]=1
 my_array3[2]=2
-echo "my_array3 ${my_array3[@]}"
+array_print my_array3
 
 # just assign multiple values
 my_array4=(0 1 2)
-echo "my_array4 ${my_array4[@]}"
+array_print my_array4
 
 # add values
 my_array5+=(0)
 my_array5+=(1)
 my_array5+=(2)
-echo "my_array5 ${my_array5[@]}"
+array_print my_array5
 
 # assign single values
 my_array6[0]=0
 my_array6[1]=1
 my_array6[2]=2
-echo "my_array6 ${my_array6[@]}"
+array_print my_array6
 
 # the two next lines are both wrong
 # x=(56,23,14)
