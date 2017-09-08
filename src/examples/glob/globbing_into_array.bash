70f61a19a043ef55ebaa7b645fa2e8a0dfe78f7a
Mark Veltzer <mark.veltzer@gmail.com>
2017-09-08 17:39:40 +0300

    more stuff

--- src/examples/glob/globbing_into_array.bash
+++ src/examples/glob/globbing_into_array.bash
@@ -4,12 +4,20 @@
 # into a bash array.
 #
 # NOTES:
+# - the best method is the first one but the disadvantage
+# is that if there are no matches then it gives an array
+# with the pattern
 # - copmgen -G must be given it's argument in quotes (otherwise
 # bash globbing will kick in).
 # - the output of bash's glob is sorted while the output of
 # compgen -G isn't.
+# - the problem with copmgen is that you must run it in a subshell
+# to collect it's output and this is slow.
 
 A=(/etc/a*)
+echo "${A[@]}"
+
+A=(/etc/notmatch*)
 echo "${A[@]}"
 
 # this approach does not work as compgen output is via stdout
