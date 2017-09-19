cbf1aa3a59218b680ac6b0c0593039339c29385a
Mark Veltzer <mark.veltzer@gmail.com>
2017-09-19 19:07:37 +0300

    some more examples

--- src/examples/redirection/capture_stderr_into_variable.bash
+++ src/examples/redirection/capture_stderr_into_variable.bash
@@ -7,8 +7,15 @@
 # - https://stackoverflow.com/questions/3130375/bash-script-store-stderr-in-a-variable
 
 # capture only the standard error of a process into a bash variable and throw stdout away
-var="$(ls -l /yy /var 2>&1 >/dev/null)"
-echo "var is [$var]"
+ls -l /notexists 2>&1 >/dev/null
+var=$(ls -l /notexists 2>&1 >/dev/null)
+echo "hoho"
+if [ "$var" != "cat: /notexists: No such file or directory" ]
+then
+	echo "ERROR"
+fi
+
+echo "here"
 
 # capture both stdout and stderr (they are interleaved)
 var="$(ls -l /yy /var 2>&1)"
---
+++ src/examples/redirection/print_to_stderr.bash
@@ -0,0 +1,17 @@
+#!/bin/bash -ue
+
+# This example shows how to print to stderr in bash.
+#
+# References:
+# - https://stackoverflow.com/questions/2990414/echo-that-outputs-to-stderr
+
+>&2 echo "this goes to stderr"
+echo "this goes to stdout"
+1>&2 echo "this goes to stderr"
+echo "this goes to stdout"
+echo "this goes to stderr" >&2
+echo "this goes to stdout"
+echo "this goes to stderr" 1>&2
+echo "this goes to stdout"
+echo "this goes to stderr" > /dev/stderr
+echo "this goes to stdout" > /dev/stdout
