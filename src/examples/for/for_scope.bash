acb4c394867b7b70207a3e22b565e8b62425ef14
Mark Veltzer <mark.veltzer@gmail.com>
2017-09-26 15:29:03 +0300

    a good example

---
+++ src/examples/for/for_scope.bash
@@ -0,0 +1,37 @@
+#!/bin/bash -ue
+
+# This example examines the scope of a variable first defined
+# a for loop in bash
+# We examine whether the file will be a global variable or not.
+#
+# Results:
+# - we indeed need to declare the variable as local before
+# the loop if we want it to have local scope.
+
+function loop_over_no_local() {
+	arr=(red "yellow green" blue)
+	for x in "${arr[@]}"
+	do
+		:
+	done
+}
+
+function loop_over_local() {
+	arr=(red "yellow green" blue)
+	local y
+	for y in "${arr[@]}"
+	do
+		:
+	done
+}
+
+loop_over_no_local
+if [ "$x" != "blue" ]
+then
+	echo "ERROR"
+fi
+loop_over_local
+if ! [ -z ${y+x} ]
+then
+	echo "ERROR"
+fi
