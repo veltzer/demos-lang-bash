cf0ee0c4c4a15e9981a548c0da289bd146a7b7e7
Mark Veltzer <mark.veltzer@gmail.com>
2017-09-28 19:28:59 +0300

    more bash examples

---
+++ src/examples/logical_operators/precedence.bash
@@ -0,0 +1,11 @@
+#!/bin/bash -ue
+
+# This example explores the precedence of logical operators in bash.
+#
+# References:
+# - https://unix.stackexchange.com/questions/88850/precedence-of-the-shell-logical-operators
+
+false || true
+echo "1"
+false || { true && false; }
+echo "after"
--- src/examples/logical_operators/short_circuit.bash
+++ src/examples/logical_operators/short_circuit.bash
@@ -1,13 +1,22 @@
 #!/bin/bash -ue
 
 # This example explores the short circuit feature of bash boolean operators.
+#
 # Notes:
 # - bash does have a short circuit feature.
 # - sometimes short circuiting is what you want.
 # - if this is not the case then make sure to execute both parts
 # of the logical condition and only then evaluate the results.
+#
+# A weird this about this example. The third command actually returns a false
+# error code but the script continues to run although we are running in error
+# mode where if an error occurs the script should stop.
 
 true || echo "where is this?"
+echo $?
 false || echo "you will see this"
+echo $?
 false && echo "where is this?"
+echo $?
 true && echo "you will see this"
+echo $?
