3693422b9a4993153fe7616785ed46add1a5580f
Mark Veltzer <mark.veltzer@gmail.com>
2017-10-02 23:47:30 +0300

    another bash example

---
+++ src/examples/functions/no_comments_in_function_defs.bash
@@ -0,0 +1,12 @@
+#!/bin/bash -ue
+
+# This example shows that when you define a function in bash
+# and then ask about it, you will not see the comments.
+
+function function_with_comments() {
+	# this is a comment
+	echo "hello, world!"
+	# this is another comment
+}
+
+type function_with_comments
