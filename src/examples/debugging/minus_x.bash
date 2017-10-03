a7ff60664790812c9b688128170460a0ff6e52fc
Mark Veltzer <mark.veltzer@gmail.com>
2017-10-03 13:31:36 +0300

    more notes

--- src/examples/debugging/minus_x.bash
+++ src/examples/debugging/minus_x.bash
@@ -1,4 +1,14 @@
 #!/bin/bash -uvx
 
+# This example shows how to make a bash script verbose.
+# The idea is to turn on the -x flag.
+# This could be done at any point in the script using 'set -x'
+# to turn debugging on and 'set +x' to turn debugging off.
+# or this could be done as in this example, at the shbang line.
+# You can also customize what will be printed before the lines
+# being executed using the 'PS4' variable as below.
+# In the following example each line will be preceeded with it's
+# line number.
+
 PS4='$LINENO '
 echo "Hello, World!"
