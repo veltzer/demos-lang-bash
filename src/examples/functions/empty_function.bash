1c28262004ac710d7b7cca9e94b3b0eeef185c3f
Mark Veltzer <mark.veltzer@gmail.com>
2017-10-03 07:13:48 +0300

    improve example

--- src/examples/functions/empty_function.bash
+++ src/examples/functions/empty_function.bash
@@ -4,6 +4,8 @@
 # The main point is that you cannot write a completely empty
 # function in bash as it is a syntax error. If you want an empty
 # function then use the ':' bash builtin which does nothing.
+# You may think that you can get away by writing a function that
+# contains only comments. You cant.
 #
 # References:
 # - https://stackoverflow.com/questions/30998558/empty-function-in-bash
