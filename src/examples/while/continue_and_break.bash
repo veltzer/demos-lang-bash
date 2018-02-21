8292ba63d241bd20892afed9ae6fc0097edf4a72
Mark Veltzer <mark.veltzer@gmail.com>
2018-02-21 18:46:10 +0200

    more notes

---
+++ src/examples/while/continue_and_break.bash
@@ -0,0 +1,22 @@
+#!/bin/bash -u
+
+# This example shows how to use 'continue' and 'break' in a bash 'while' loop.
+#
+# References:
+# - http://tldp.org/LDP/Bash-Beginners-Guide/html/sect_09_05.html
+
+x=0
+while true
+do
+	let "x=x+1"
+	if [[ "$x" -eq 1000 ]]
+	then
+		break
+	fi
+	let "y=x%2"
+	if [[ "$y" -eq 1 ]]
+	then
+		continue
+	fi
+	echo "$x"
+done
