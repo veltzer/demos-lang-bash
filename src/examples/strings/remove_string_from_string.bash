9052be9bab0660e9699796a1238f27dbf328e361
Mark Veltzer <mark.veltzer@gmail.com>
2018-02-21 19:05:45 +0200

    another example

---
+++ src/examples/strings/remove_string_from_string.bash
@@ -0,0 +1,15 @@
+#!/bin/bash -u
+
+# This is an example of how to remove a string from another string.
+#
+# TODO:
+# I still need to explain how this works as regard the spaces.
+#
+# References:
+# - https://stackoverflow.com/questions/20783187/remove-the-all-occurences-of-a-word-from-a-string-in-bash-shell-linux?lq=1
+
+str='one two one three one'
+
+removed="${str//one/}"
+
+echo $removed
