a4feb2ccf5d12a19aef84f63568e98f2ef6c42ad
Mark Veltzer <mark.veltzer@gmail.com>
2018-05-22 19:37:03 +0300

    more examples

---
+++ src/examples/command_line/command_line_pass_some.bash
@@ -0,0 +1,13 @@
+#!/bin/bash -u
+
+# this is an example of getting specific arguments from the command line.
+
+if [[ $# -lt 1 ]]
+then
+	echo "usage: script [must_pass] [optionals...]"
+	exit 1
+fi
+must_pass_param=$1
+echo "must_pass_param is $must_pass_param"
+shift
+./count_them.bash $@
