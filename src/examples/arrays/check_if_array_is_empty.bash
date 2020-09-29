ed1c3699136fc2b0cf075f2d32c8873574afc26c
Mark Veltzer <mark.veltzer@gmail.com>
2020-09-29 07:19:40 +0300

    another example

---
+++ src/examples/arrays/check_if_array_is_empty.bash
@@ -0,0 +1,27 @@
+#!/bin/bash -u
+
+# This example shows how to check if an array is empty
+#
+# References:
+# - https://serverfault.com/questions/477503/check-if-array-is-empty-in-bash/477506
+
+array_with_elements=(
+	"one two"
+	"five six"
+	"three four"
+)
+array_without_elements=(
+)
+
+if [ ${#array_with_elements[@]} -eq 0 ]
+then
+	echo "array is empty"
+else
+	echo "array is not empty"
+fi
+if [ ${#array_without_elements[@]} -eq 0 ]
+then
+	echo "array is empty"
+else
+	echo "array is not empty"
+fi
