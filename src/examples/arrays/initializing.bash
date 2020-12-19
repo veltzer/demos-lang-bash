9870147e9ff049db91f8af4cf3366cdf587ef3f4
Mark Veltzer <mark.veltzer@gmail.com>
2020-12-19 09:07:30 +0200

    more fixes

--- src/examples/arrays/initializing.bash
+++ src/examples/arrays/initializing.bash
@@ -20,36 +20,36 @@
 source src/includes/array.bashinc
 
 # declare an array (it will be empty)
-declare -A my_array1
+declare -a my_array1
 array_print my_array1
 
 # declare an array and put data
-declare -A my_array2[0]=0
-declare -A my_array2[1]=1
-declare -A my_array2[2]=2
+declare -a my_array2[0]="0 value"
+declare -a my_array2[1]="1 value"
+declare -a my_array2[2]="2 value"
 array_print my_array2
 
 # declare an array and then assign
-declare -A my_array3
-my_array3[0]=0
-my_array3[1]=1
-my_array3[2]=2
+declare -a my_array3
+my_array3[0]="0 value"
+my_array3[1]="1 value"
+my_array3[2]="2 value"
 array_print my_array3
 
 # just assign multiple values
-my_array4=(0 1 2)
+my_array4=("0 value" "1 value" "2 value")
 array_print my_array4
 
 # add values
-my_array5+=(0)
-my_array5+=(1)
-my_array5+=(2)
+my_array5+=("0 value")
+my_array5+=("1 value")
+my_array5+=("2 value")
 array_print my_array5
 
 # assign single values
-my_array6[0]=0
-my_array6[1]=1
-my_array6[2]=2
+my_array6[0]="0 value"
+my_array6[1]="1 value"
+my_array6[2]="2 value"
 array_print my_array6
 
 # the two next lines are both wrong
@@ -57,7 +57,7 @@ array_print my_array6
 # #x=[56,23,14]
 
 echo "assign from variable"
-my_var="0 1 2"
+my_var="0 value 1 value 2 value"
 my_array7=($my_var)
 array_print my_array7
 
