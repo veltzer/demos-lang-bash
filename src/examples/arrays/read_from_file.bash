e3d1936e668ac88c3a3b64e9b28002b6931dfe04
Mark Veltzer <mark.veltzer@gmail.com>
2021-01-08 04:17:06 +0200

    examples about reading file into array in bash

--- src/examples/arrays/read_from_file.bash
+++ src/examples/arrays/read_from_file.bash
@@ -1,15 +1,36 @@
 #!/bin/bash -u
 
 # This example shows how to read an array from a file.
-# It reads the lines in '/etc/group' into an array.
+# Note:
+# - The recommended way is using the 'mapfile' builtin.
+# - Look for help on the 'mapfile' builtin in "man builtins"
+# - Dont use "read -a" as it is hard to get working right (see below).
 #
 # References:
 # - https://stackoverflow.com/questions/1688999/how-can-i-read-a-list-of-filenames-from-a-file-in-bash
+
+filename="data/list_of_values.txt"
 
 my_array=()
 while read F
 do
 	my_array+=($F)
-done < /etc/group
+done < "$filename"
+echo $?
+declare -p my_array
 
-echo "${my_array[@]}"
+readarray -t my_array_2 < "$filename"
+echo $?
+declare -p my_array_2
+
+mapfile -t my_array_3 < "$filename"
+echo $?
+declare -p my_array_3
+
+read -a my_array_4 -d $'\n' < "$filename"
+echo $?
+declare -p my_array_4
+
+IFS=$'\n' read -d '' -r -a my_array_5 < "$filename" 
+echo $?
+declare -p my_array_5
