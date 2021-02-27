2d474b258d0ce2894f77c26173b2a434ea226243
Mark Veltzer <mark.veltzer@gmail.com>
2021-02-27 21:54:25 +0200

    example about looping over path components

---
+++ src/examples/paths/loop_over_path_components.bash
@@ -0,0 +1,13 @@
+#!/bin/bash -u
+
+# This example shows how to loop over path components
+#
+# References:
+# - https://stackoverflow.com/questions/11655770/looping-through-the-elements-of-a-path-variable-in-bash
+
+for path in ${PATH//:/ }; do
+	if [ -d "$path" ]
+	then
+		echo "$path"
+	fi
+done
