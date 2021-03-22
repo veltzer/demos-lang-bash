fa426e1ff9bfd77881077a4887e0b29d3756e8aa
Mark Veltzer <mark.veltzer@gmail.com>
2021-03-23 01:47:51 +0200

    more examples

---
+++ src/examples/if/if_with_variable.bash
@@ -0,0 +1,19 @@
+#!/bin/bash -u
+
+# This is an example of how to write an "if" statement in bash which checks whether a variable
+# has a postive or negative value.
+# The trick is to use the 'true' and 'false' programs.
+# 'true' returns a positive error code (0)
+# 'false' returns a negative error code (!=0)
+
+debug=true
+if $debug
+then
+	echo "This is a debug message"
+fi
+
+turn_on=false
+if $turn_on
+then
+	echo "You should not see this..."
+fi
