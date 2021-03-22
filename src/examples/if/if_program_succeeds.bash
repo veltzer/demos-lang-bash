d3503fed5f6df2717ae3a9c534a1e36c46e4424c
Mark Veltzer <mark.veltzer@gmail.com>
2021-03-23 01:47:51 +0200

    more examples

---
+++ src/examples/if/if_program_succeeds.bash
@@ -0,0 +1,18 @@
+#!/bin/bash -u
+
+# This example shows the best way to check if a program fails in bash
+# Notes:
+# - you must collect the return code *right after* the running of the program.
+# This is because any program run after it will change this return code ($?).
+# - if you want to check if a program succeeds it easier since you can just
+# do: if [program].
+#
+# References:
+# - https://askubuntu.com/questions/29370/how-to-check-if-a-command-succeeded
+
+true
+code=$?
+if [ $code -eq 0 ]
+then
+	echo "yes, program succeeded"
+fi
