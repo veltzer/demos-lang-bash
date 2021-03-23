e1f5c219abf637963b33af464c129162015db8ce
Mark Veltzer <mark.veltzer@gmail.com>
2021-03-23 10:01:05 +0200

    good samples

---
+++ src/examples/redirection/switch_stdout_and_stderr.bash
@@ -0,0 +1,12 @@
+#!/bin/bash -u
+
+# This example shows how you can switch stdout and stderr for a program.
+# This is useful in a cade in which you want to pipe errors out and not output.
+#
+# References:
+# - https://stackoverflow.com/questions/13299317/io-redirection-swapping-stdout-and-stderr
+
+echo we will now see errors instead of output
+find /etc 2> /dev/null 3>&2 2>&1 1>&3 | head
+echo simpler 
+find /etc 2>&1 1> /dev/null | head
