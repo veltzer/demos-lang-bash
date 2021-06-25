12074fc3dc427e17e1d312ed97a300f5b4ec7061
Mark Veltzer <mark.veltzer@gmail.com>
2021-06-25 12:32:01 +0300

    another example

--- src/examples/strings/iterate_string_characters_by_charcter.bash
+++ src/examples/strings/iterate_string_characters_by_charcter.bash
@@ -5,9 +5,18 @@
 string='encyclopedia'
 
 len="${#string}"
+
+# with a simple while loop
 ((i=0))
 while [ "$i" -lt "$len" ]
 do
 	echo "${string:$i:1}"
 	((i++))
+done
+
+echo "==============================="
+# with seq(1)
+for i in $(seq 0 $len)
+do
+	echo "${string:$i:1}"
 done
