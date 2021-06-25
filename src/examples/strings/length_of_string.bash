92383d764e4bb0048bf0af376ebecac04391a1b7
Mark Veltzer <mark.veltzer@gmail.com>
2021-06-25 12:27:28 +0300

    fixing warnings

--- src/examples/strings/length_of_string.bash
+++ src/examples/strings/length_of_string.bash
@@ -9,7 +9,10 @@ strvar='hello world'
 
 # first is the best way and probably fastest
 echo "${#strvar}"
+# shellcheck disable=SC2003
 expr length "$strvar"
 expr "${strvar}" : '.*'
+# who do you do this with (()) 'expr' is antiquated and 
+# ((length "$strvar"))
 echo -n "$strvar" | wc -c
 echo -n "$strvar" | awk '{print length}'
