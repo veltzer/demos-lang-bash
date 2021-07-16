f88aae3149b3c7d5dc5f2b13a15df36dac722292
Mark Veltzer <mark.veltzer@gmail.com>
2021-07-16 18:19:43 +0300

    example of how to check if ports are open

---
+++ src/examples/networking/check_if_port_is_taken.bash
@@ -0,0 +1,16 @@
+#!/bin/bash -u
+
+# This example shows you how to check if a port is busy in bash
+# using the ss(1) command
+# It's better than the referenced link.
+#
+# References:
+# - https://www.cyberciti.biz/faq/how-do-i-check-if-a-port-is-in-use-on-linux/
+
+port_name=$1
+if [[ $(ss --no-header --tcp --listening "sport = :$1") ]]
+then
+	echo "port [$1] is busy"
+else
+	echo "port [$1] is not busy"
+fi
