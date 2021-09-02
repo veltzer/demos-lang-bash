02fb3f3f26ae6a61122c3c3a8a6254df2b22d505
Mark Veltzer <mark.veltzer@gmail.com>
2021-09-02 19:48:02 +0300

    typos

--- src/examples/core/comments/multi_line_comment.bash
+++ src/examples/core/comments/multi_line_comment.bash
@@ -37,7 +37,7 @@ $$foo is fine, not because it is not evaluated but rather because
 $$ is seen as the variable (current shell pid).
 - it will allow single quotes (') (good)
 - you can continue the comment until COMMENT appears (good)
-- chages $? (bad)
+- changes $? (bad)
 
 $$foo (single $ will cause an error, bad)
 ${a=5} (evaluated, bad)
@@ -53,7 +53,7 @@ false
 - wont evaluate the content of the comment (good)
 - cannot use single quotes (bad)
 - the comment continues until a single quote (bad)
-- chages $? (bad)
+- changes $? (bad)
 
 $foo (no error, good)
 ${a=6} (not evaluated, good)
@@ -68,7 +68,7 @@ false
 - wont evaluate the content of the comment (good)
 - it will allow single quotes (') (good)
 - you can continue the comment until COMMENT appears (good)
-- chages $? (bad)
+- changes $? (bad)
 
 $foo (no error, good)
 ${a=7} (not evaluated, good)
@@ -85,7 +85,7 @@ false
 - wont evaluate the content of the comment (good)
 - it will allow single quotes (') (good)
 - you can continue the comment until COMMENT appears (good)
-- chages $? (bad)
+- changes $? (bad)
 
 $foo (no error, good)
 ${a=8} (not evaluated, good)
