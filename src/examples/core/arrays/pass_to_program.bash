8dc4617ba44516a3fb2970075b5bb1f90a63a7ec
Mark Veltzer <mark.veltzer@gmail.com>
2021-10-05 16:09:32 +0300

    more notes

--- src/examples/core/arrays/pass_to_program.bash
+++ src/examples/core/arrays/pass_to_program.bash
@@ -2,6 +2,7 @@
 
 # This example explores how to pass an array as arguments to a program
 # on the command line
+# You can see from the output of cat that indeed cat is passed three arguments
 
 my_array=(
 	"first value"
