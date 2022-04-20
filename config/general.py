1fe549b47ed83e9bdf79dfa69a8eff4497e1ea99
Mark Veltzer <mark.veltzer@gmail.com>
2022-04-20 19:28:22 +0300

    better templating code

--- config/general.py
+++ config/general.py
@@ -2,7 +2,7 @@ import datetime
 import os
 import socket
 
-general_current_folder = os.path.basename(os.getcwd())
-general_current_year = datetime.datetime.now().year
-general_homedir = os.path.expanduser("~")
-general_hostname = socket.gethostname()
+current_folder = os.path.basename(os.getcwd())
+current_year = datetime.datetime.now().year
+homedir = os.path.expanduser("~")
+hostname = socket.gethostname()
