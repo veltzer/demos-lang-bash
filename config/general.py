44bf0751fd74d12bd1662a7f5b526d2ae29128e0
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-19 21:09:45 +0300

    Better templates

--- config/general.py
+++
@@ -1,8 +0,0 @@
-import datetime
-import os
-import socket
-
-current_folder = os.path.basename(os.getcwd())
-current_year = datetime.datetime.now().year
-homedir = os.path.expanduser("~")
-hostname = socket.gethostname()
