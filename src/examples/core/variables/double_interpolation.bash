f18c5f495ac67129af7998cd7a1550a349de52a8
Mark Veltzer <mark.veltzer@gmail.com>
2022-07-31 16:31:12 +0300

    no commit message given

---
+++ src/examples/core/variables/double_interpolation.bash
@@ -0,0 +1,16 @@
+#!/bin/bash -u
+
+# this example shows how to interpolate variables in bash
+#
+# References:
+# - https://www.linuxshelltips.com/difference-between-and-in-bash/
+
+x=7
+echo "x is $x"
+echo "x is ${x}"
+
+y=x
+echo "y is $y"
+# this double interpolation does not work
+echo "$y is $$y"
+echo "double intepolation of y is ${!y}"
