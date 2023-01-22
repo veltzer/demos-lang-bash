94de87b78dbd5bb0859bf876c21d3fb718700d25
Mark Veltzer <mark.veltzer@gmail.com>
2023-01-22 15:18:25 +0200

    no commit message given

--- src/examples/core/shopt/save_and_restore_option_state.sh
+++ src/examples/core/shopt/save_and_restore_option_state.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -ux
 
+# Sometimes you want to write a piece of code and make sure that some bash options
+# are set in a certain way in it (-e, -x etc)
+# The problem is how to return the options to the way they were before the piece of code started?
+#
+# References:
+# - https://unix.stackexchange.com/questions/310957/how-to-restore-the-value-of-shell-options-like-set-x
+
 set -e
 echo "a"
 state_of_errexit=$(shopt -p -o errexit)
