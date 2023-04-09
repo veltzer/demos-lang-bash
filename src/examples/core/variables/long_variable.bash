0b37e7d88746b40f2c1530a387b34804f4cf747d
Mark Veltzer <mark.veltzer@gmail.com>
2023-04-09 10:57:18 +0300

    no commit message given

---
+++ src/examples/core/variables/long_variable.bash
@@ -0,0 +1,35 @@
+#!/bin/bash -u
+
+# This is an example of how to do long variables in bash
+
+read_it() {
+	VAR1=$(cat)
+}
+read_it <<EOF
+first line
+second line
+third line
+EOF
+echo "$VAR1"
+
+IFS='' read -d '' -r VAR2 <<EOF
+first line
+second line
+third line
+EOF
+echo "$VAR2"
+
+nl="
+"
+read_heredoc(){
+	VAR3=""
+	while IFS="$nl" read -r line; do
+		VAR3="$VAR3$line$nl"
+	done 
+}
+read_heredoc <<EOF
+first line
+second line
+third line
+EOF
+echo "$VAR3"
