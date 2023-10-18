a340084f71023bc2e89c39459776c52a84a69d31
Mark Veltzer <mark.veltzer@gmail.com>
2023-10-18 18:44:49 +0300

    no commit message given

--- src/examples/core/logging/conditional_logging.bash
+++ src/examples/core/logging/conditional_logging.bash
@@ -1,6 +1,8 @@
 #!/bin/bash -u
 
 # This example shows how to do conditional logging in bash
+# References:
+# - https://stackoverflow.com/questions/8455991/elegant-way-for-verbose-mode-in-scripts
 
 function log () {
     if [[ "${V}" -eq 1 ]]; then
