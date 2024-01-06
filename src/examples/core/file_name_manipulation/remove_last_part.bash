1888e353b51cfe2df02a24ced04b47b410eaf5c0
Mark Veltzer <mark.veltzer@gmail.com>
2024-01-06 16:43:38 +0200

    another example

--- src/examples/core/file_name_manipulation/remove_last_part.bash
+++ src/examples/core/file_name_manipulation/remove_last_part.bash
@@ -1,7 +1,8 @@
 #!/bin/bash -u
 
-# This is an example of how to remove the last part of a filename. The better
-# way is not to use the 'dirname(1)' utility but rather to use the bash built-in syntax.
+# This is an example of how to remove the last part of a filename.
+# The better way is not to use the 'dirname(1)' utility but rather to use the bash
+# built-in string manipulation syntax.
 # References:
 # - https://unix.stackexchange.com/questions/28771/how-to-remove-last-part-of-a-path-in-bash
 
