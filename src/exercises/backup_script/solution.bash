e343c37b1c9458bf42cdc5f42cef02e029d8acc3
Mark Veltzer <mark.veltzer@gmail.com>
2024-07-08 12:25:26 +0300

    no commit message given

--- src/exercises/backup_script/solution.bash
+++ src/exercises/backup_script/solution.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -eu
 
 # the next line is a poor excuse for the second line...
-# tar cvfz /tmp/backup.tar.gz /etc 
+# tar --create --gzip --ignore-failed-read -f /tmp/backup.tar.gz /etc 2> /dev/null
 find /etc -type f -readable 2> /dev/null | tar --create --files-from=- --file=/tmp/backup.tar.gz --gzip --absolute-names
