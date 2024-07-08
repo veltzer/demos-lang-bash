728f59b2407050e710269d180878df3b68e2a276
Mark Veltzer <mark.veltzer@gmail.com>
2024-07-08 12:23:15 +0300

    no commit message given

---
+++ src/exercises/backup_script/exercise.md
@@ -0,0 +1,5 @@
+# Backup script
+
+Write a script that backs up all regular files in /etc (recursively).
+Use tar to back up the files.
+Do not backup symbolic links or any irregular files.
--- src/exercises/backup_script/solution.bash
+++ src/exercises/backup_script/solution.bash
@@ -1,3 +1,5 @@
 #!/bin/bash -eu
 
+# the next line is a poor excuse for the second line...
+# tar cvfz /tmp/backup.tar.gz /etc 
 find /etc -type f -readable 2> /dev/null | tar --create --files-from=- --file=/tmp/backup.tar.gz --gzip --absolute-names
