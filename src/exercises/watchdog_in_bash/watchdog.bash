5ebc8d5577c7b704bef09e0594fd97e29fffed35
Mark Veltzer <mark.veltzer@gmail.com>
2024-12-22 10:14:16 +0200

    no commit message given

---
+++ src/exercises/watchdog_in_bash/watchdog.bash
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+while true
+do
+	echo "running the monitored process..."
+	set +e
+	./process_to_monitor.sh
+	return_code=$?
+	set -e
+	if [ ${return_code} != 0 ]
+	then
+		echo "the child failed with exit code ${return_code}..."
+	fi
+done
